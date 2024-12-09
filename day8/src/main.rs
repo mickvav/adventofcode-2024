19b678268ad50c481c5e184e6c435c25bfcad270
Mihail Vasilev <922814+mickvav@users.noreply.github.com>
2024-12-09 22:08:37 +0000

    day8 p 2

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,9 +1,8 @@
 use std::collections::HashMap;
-use std::fs;
 use std::fmt;
+use std::fs;
 
-
-#[derive(Debug, Eq, Hash, PartialEq)]
+#[derive(Debug, Eq, Hash, PartialEq, Clone)]
 struct Pos {
     r: usize,
     c: usize,
@@ -60,7 +59,6 @@ impl Inp {
     fn metrics(&self) -> usize {
         let mut loc: HashMap<Pos, u8> = HashMap::new();
         for (u, v) in self.pos.iter() {
-            println!("> {} {}", u, v.len());
             for p1 in v {
                 for p2 in v {
                     if p1 != p2 {
@@ -68,7 +66,6 @@ impl Inp {
                         let (b2, c1) = boxedshift(p1.c, p2.c, self.cols);
                         if b1 && b2 {
                             loc.entry(Pos { r: r1, c: c1 }).or_insert(*u);
-                            println!("{} {}", r1, c1)
                         }
                     }
                 }
@@ -77,7 +74,44 @@ impl Inp {
         return loc.len();
     }
     fn metrics2(&self) -> usize {
-        return 0;
+        let mut loc: HashMap<Pos, u8> = HashMap::new();
+        for (u, v) in self.pos.iter() {
+            for p1 in v {
+                for p2 in v {
+                    if p1 != p2 {
+                        let mut p1i: Pos = p1.clone();
+                        let mut p2i: Pos= p2.clone();
+                        let mut keepgoing = true;
+                        loc.entry(p1.clone()).or_insert(*u);
+                        loc.entry(p2.clone()).or_insert(*u);
+                        while keepgoing {
+                            let (b1, r1) = boxedshift(p1i.r, p2i.r, self.rows);
+                            let (b2, c1) = boxedshift(p1i.c, p2i.c, self.cols);
+                            if b1 && b2 {
+                                loc.entry(Pos { r: r1, c: c1 }).or_insert(*u);
+                                p1i.r = p2i.r;
+                                p1i.c = p2i.c;
+                                p2i.r = r1;
+                                p2i.c = c1;
+                            } else {
+                                keepgoing = false;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        for r in 0..self.rows {
+            for c in 0..self.cols {
+                if loc.contains_key(&Pos{r: r, c: c}) {
+                    print!("#");
+                } else {
+                    print!(".");
+                }
+            }
+            println!();
+        }
+        return loc.len();
     }
 }
 
@@ -88,7 +122,7 @@ mod tests {
     #[test]
     fn it_works() {
         assert_eq!(read_file("test.txt").metrics(), 14);
-        assert_eq!(read_file("test.txt").metrics2(), 0);
+        assert_eq!(read_file("test.txt").metrics2(), 34);
     }
 }
 
